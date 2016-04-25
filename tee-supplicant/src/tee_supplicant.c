d08905c4af17e0d57b6e10d58be873bd1ee03bcd
Victor Chong <victor.chong@linaro.org>
2016-04-25 12:05:15 +0900

    tee-supplicant: prevent double load
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- tee-supplicant/src/tee_supplicant.c
+++ tee-supplicant/src/tee_supplicant.c
@@ -360,11 +360,84 @@ out:
 	OUTMSG();
 }
 
+pid_t proc_find(const char* name)
+{
+	DIR* dir;
+	struct dirent* ent;
+	char* endptr;
+	char buf[512];
+
+	if (!(dir = opendir("/proc")))
+	{
+		EMSG("can't open /proc");
+		return -1;
+	}
+
+	while((ent = readdir(dir)) != NULL)
+	{
+		/*
+		 * if endptr is not a null character, the directory is not
+		 * entirely numeric, so ignore it
+		 */
+		long lpid = strtol(ent->d_name, &endptr, 10);
+		if (*endptr != '\0')
+		{
+			continue;
+		}
+
+		/* try to open the cmdline file */
+		snprintf(buf, sizeof(buf), "/proc/%ld/cmdline", lpid);
+		FILE* fp = fopen(buf, "r");
+
+		if (fp)
+		{
+			if (fgets(buf, sizeof(buf), fp) != NULL)
+			{
+				/* check the first token in the file, the program name */
+				char* first = strtok(buf, " ");
+				if (!strcmp(first, name))
+				{
+					fclose(fp);
+					closedir(dir);
+					return (pid_t)lpid;
+				}
+			}
+			fclose(fp);
+		}
+	}
+
+	closedir(dir);
+	return -1;
+}
+
 int main(int argc, char *argv[])
 {
 	int fd;
 	union tee_rpc_invoke request;
 	int ret;
+	FILE* fp;
+
+	/* If tee-supplicant already running, let user know and exit without loading. */
+	if ((fp = popen ("ps | grep tee-supplicant 2>&1", "r")) != NULL)
+	{
+		char cmd_output[1024];
+		char *first_occurrence;
+		char *second_occurrence;
+
+		/* Store output of above command in buffer. */
+		fread (cmd_output, 1, sizeof(cmd_output), fp);
+		pclose (fp);
+
+		first_occurrence = strstr (cmd_output, argv[0]);
+		second_occurrence = strstr (first_occurrence+1, argv[0]);
+
+		/* If "tee-supplicant" appears twice in buffer, it means it's already running. */
+		if (second_occurrence)
+		{
+			IMSG ("tee-supplicant already running.. exiting without loading\n");
+			return EXIT_FAILURE;
+		}
+	}
 
 	if (argc > 2)
 		return usage();
