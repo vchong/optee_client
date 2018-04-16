ce021ff9cdb14c11dc1144933b7510918eb9152a
Victor Chong <victor.chong@linaro.org>
2018-04-16 05:28:06 +0100

    Android.mk: Add libsks (liboptee_cryptoki) build
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 ################################################################################
-# Android optee-client and optee-supplicant makefile                                                #
+# Android optee-client, libsks and tee-supplicant makefile                     #
 ################################################################################
 LOCAL_PATH := $(call my-dir)
 
@@ -27,7 +27,7 @@ endif
 LOCAL_CFLAGS += -DDEBUGLEVEL_$(CFG_TEE_CLIENT_LOG_LEVEL)
 LOCAL_CFLAGS += -DBINARY_PREFIX=\"TEEC\"
 
-LOCAL_SRC_FILES := libteec/src/tee_client_api.c\
+LOCAL_SRC_FILES := libteec/src/tee_client_api.c \
                    libteec/src/teec_trace.c
 ifeq ($(CFG_TEE_BENCHMARK),y)
 LOCAL_CFLAGS += -DCFG_TEE_BENCHMARK
@@ -35,10 +35,36 @@ LOCAL_SRC_FILES += teec_benchmark.c
 endif
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/public \
-                    $(LOCAL_PATH)/libteec/include \
+                    $(LOCAL_PATH)/libteec/include
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE := libteec
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/public
+
+include $(BUILD_SHARED_LIBRARY)
+
+# Build libsks, i.e. liboptee_cryptoki.so
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS += $(optee_CFLAGS)
+
+LOCAL_SRC_FILES := libsks/src/pkcs11_api.c \
+                   libsks/src/ck_debug.c \
+                   libsks/src/ck_helpers.c \
+                   libsks/src/invoke_ta.c \
+                   libsks/src/pkcs11_token.c \
+                   libsks/src/serializer.c \
+                   libsks/src/serialize_ck.c \
+                   libsks/src/pkcs11_processing.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/public \
+                    $(LOCAL_PATH)/libsks/include
+
+LOCAL_SHARED_LIBRARIES := libteec
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE := liboptee_cryptoki
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/public
