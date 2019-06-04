cf7c607f6bf2f4f637f69eac2764d79011694f7c
Victor Chong <victor.chong@linaro.org>
2019-06-04 08:44:56 +0100

    set log levels to 4
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- config.mk
+++ config.mk
@@ -11,12 +11,12 @@
 # CFG_TEE_CLIENT_LOG_LEVEL
 #   Client (User Non Secure) log level
 #   Supported values: 0 (no traces) to 4 (all traces)
-CFG_TEE_CLIENT_LOG_LEVEL?=1
+CFG_TEE_CLIENT_LOG_LEVEL?=4
 
 # CFG_TEE_SUPP_LOG_LEVEL
 #   Supplicant log level
 #   Supported values: 0 (no traces) to 4 (all traces)
-CFG_TEE_SUPP_LOG_LEVEL?=1
+CFG_TEE_SUPP_LOG_LEVEL?=4
 
 # CFG_TEE_FS_PARENT_PATH
 #   Path to folder that will contain TEE filesystem.
