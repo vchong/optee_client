b2f8d1d443a946513b81e57c2c7e0b401a319864
Victor Chong <victor.chong@linaro.org>
2020-05-12 11:12:19 +0100

    aosp_clang_dev_wip

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 ################################################################################
-# Android optee-client and optee-supplicant makefile                                                #
+# Android optee_client, libckteec and tee-supplicant makefile                  #
 ################################################################################
 LOCAL_PATH := $(call my-dir)
 
@@ -31,7 +31,7 @@ endif
 LOCAL_CFLAGS += -DDEBUGLEVEL_$(CFG_TEE_CLIENT_LOG_LEVEL)
 LOCAL_CFLAGS += -DBINARY_PREFIX=\"TEEC\"
 
-LOCAL_SRC_FILES := libteec/src/tee_client_api.c\
+LOCAL_SRC_FILES := libteec/src/tee_client_api.c \
                    libteec/src/teec_trace.c
 ifeq ($(CFG_TEE_BENCHMARK),y)
 LOCAL_CFLAGS += -DCFG_TEE_BENCHMARK
@@ -39,7 +39,7 @@ LOCAL_SRC_FILES += teec_benchmark.c
 endif
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/public \
-                    $(LOCAL_PATH)/libteec/include \
+                    $(LOCAL_PATH)/libteec/include
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE := libteec
@@ -47,9 +47,47 @@ LOCAL_MODULE := libteec
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 
+# Build the 32-bit and 64-bit versions.
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/public
 
 include $(BUILD_SHARED_LIBRARY)
 
-# TEE Supplicant
+################################################################################
+# Build libckteec.so                                                           #
+################################################################################
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS += $(optee_CFLAGS)
+
+LOCAL_SRC_FILES := libckteec/src/pkcs11_api.c \
+                   libckteec/src/ck_debug.c \
+                   libckteec/src/ck_helpers.c \
+                   libckteec/src/invoke_ta.c \
+                   libckteec/src/pkcs11_token.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/public \
+                    $(LOCAL_PATH)/libckteec/include
+
+LOCAL_SHARED_LIBRARIES := libteec
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE := libckteec
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+# Build the 32-bit and 64-bit versions.
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/libckteec/include
+
+include $(BUILD_SHARED_LIBRARY)
+
+################################################################################
+# Build TEE Supplicant                                                         #
+################################################################################
 include $(LOCAL_PATH)/tee-supplicant/tee_supplicant_android.mk
